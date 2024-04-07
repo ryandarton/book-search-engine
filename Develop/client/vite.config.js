b4f0869dc16429d9e49033ca1acd3e19ee09fd6e
Ryan Darton <ryan@timemachinevideo.com>
2024-04-07 08:53:26 -0600

    Even more node_modules.

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,9 +10,8 @@ export default defineConfig({
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
-        changeOrigin: true,
         secure: false,
-        ws: true,
+        changeOrigin: true,
       },
     },
   },
