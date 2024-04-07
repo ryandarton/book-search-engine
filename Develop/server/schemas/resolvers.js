04cedeb9f218d35a9439e339c22fd765489b134f
Ryan Darton <ryan@timemachinevideo.com>
2024-04-07 00:42:35 -0600

    Add searchBooks resolver and update saveBook resolver

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,6 +1,7 @@
-const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+const axios = require('axios');
 
 const resolvers = {
   Query: {
@@ -11,6 +12,25 @@ const resolvers = {
       }
       throw new AuthenticationError('Not logged in');
     },
+
+    searchBooks: async (_, { query }) => {
+      try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+        const { items } = response.data;
+        const bookData = items.map((book) => ({
+          bookId: book.id,
+          authors: book.volumeInfo.authors || ['No author to display'],
+          title: book.volumeInfo.title,
+          description: book.volumeInfo.description,
+          image: book.volumeInfo.imageLinks?.thumbnail || '',
+          link: book.volumeInfo.previewLink,
+        }));
+        return bookData;
+      } catch (error) {
+        console.error('Error fetching books from Google Books API:', error);
+        throw new Error('Failed to fetch books from Google Books API');
+      }
+    },
   },
 
   Mutation: {
@@ -19,36 +39,32 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
       const correctPw = await user.isCorrectPassword(password);
-
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (parent, { authors, description, title, bookId, image, link }, context) => {
+
+    saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
-        const updatedUser = await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { savedBooks: { authors, description, title, bookId, image, link } } },
-          { new: true }
-        );
+        const updatedUser = await User.findByIdAndUpdate(context.user._id, { $push: { savedBooks: bookData } }, { new: true });
+        console.log(updatedUser);
         return updatedUser;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('Error with Authentication!');
     },
+
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        const updatedUser = await User.findOneAndUpdate({ _id: context.user._id }, { $pull: { savedBooks: { bookId } } }, { new: true });
+        const updatedUser = await User.findByIdAndUpdate({ _id: context.user._id }, { $pull: { savedBooks: { bookId } } }, { new: true });
         return updatedUser;
       }
       throw new AuthenticationError('You need to be logged in!');
