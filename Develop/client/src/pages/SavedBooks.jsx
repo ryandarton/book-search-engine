7b611d0441fcf7eff880412116be4a565f265e90
Ryan Darton <ryan@timemachinevideo.com>
2024-04-07 01:54:22 -0600

    Refactor SavedBooks and SearchBooks components

--- Develop/client/src/pages/SavedBooks.jsx
+++ Develop/client/src/pages/SavedBooks.jsx
@@ -1,63 +1,52 @@
 import { useState, useEffect } from 'react';
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+import { useQuery, useMutation } from '@apollo/client';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(getMe);
+  const [removeBook] = useMutation(deleteBook);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const userData = data?.me || {};
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-        if (!token) {
-          return false;
-        }
-        const response = await getMe(token);
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
     if (!token) {
       return false;
     }
+
     try {
-      const response = await deleteBook(bookId, token);
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      await removeBook({
+        variables: { bookId },
+        update: (cache) => {
+          const { me } = cache.readQuery({ query: getMe });
+          const updatedUser = {
+            ...me,
+            savedBooks: me.savedBooks.filter((book) => book.bookId !== bookId),
+          };
+          cache.writeQuery({
+            query: getMe,
+            data: { me: updatedUser },
+          });
+        },
+      });
+
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <div fluid className='text-light bg-dark p-5'>
+      <div className='text-light bg-dark p-5'>
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -71,16 +60,21 @@ const SavedBooks = () => {
         <Row>
           {userData.savedBooks.map((book) => {
             return (
-              <Col md='4'>
-                <Card key={book.bookId} border='dark'>
+              <Col md='4' key={book.bookId}>
+                <Card border='dark'>
                   {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
+                    <div className='d-flex justify-content-between align-items-center'>
+                      <Button variant='primary' href={book.link} target='_blank' rel='noopener noreferrer'>
+                        View Book
+                      </Button>
+                      <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                        Delete this Book!
+                      </Button>
+                    </div>
                   </Card.Body>
                 </Card>
               </Col>
--- Develop/client/src/pages/SearchBooks.jsx
+++ Develop/client/src/pages/SearchBooks.jsx
@@ -7,24 +7,23 @@ import { searchGoogleBooks, saveBook } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
-  // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   const [searchBooks, { loading, data }] = useLazyQuery(searchGoogleBooks);
-  const [saveBook] = useMutation(saveBook);
+  const [saveTheBook] = useMutation(saveBook);
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   }, [savedBookIds]);
 
-  // create method to search for books and set state on form submit
+  useEffect(() => {
+    if (data) {
+      setSearchedBooks(data.searchBooks || []);
+    }
+  }, [data]);
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -40,12 +39,11 @@ const SearchBooks = () => {
     }
   };
 
-  // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
+    const { __typename, ...bookDataWithoutTypename } = bookToSave;
+
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -53,7 +51,7 @@ const SearchBooks = () => {
     }
 
     try {
-      await saveBook({ variables: { bookData: bookDataWithoutTypename } });
+      await saveTheBook({ variables: { bookData: bookDataWithoutTypename } });
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
